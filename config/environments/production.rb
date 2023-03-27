075d7bcf29ae1f12dc6fb6d63f5c5eb84edb726c
barsu5489 <ebarsulai@gmail.com>
2023-03-27 20:12:17 +0300

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-
+  config.active_record.sqlite3_production_warning=false
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
